45866e25ac229b7a9d6183337f4c990d506e148b
sajalbiswas1 <sajalb1899@gmail.com>
2024-02-22 09:50:22 +0600

    update subscribe build problem

---
+++ app/subscribe/fail/[id]/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PaymentFail = () => {
+    return (
+        <div>
+            <h2 className='text-white'>Payment fail</h2>
+        </div>
+    );
+};
+
+export default PaymentFail;
