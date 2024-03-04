f02328c8cc09cb8ce18a95271943cd7185b9e28c
Mujahid2000 <your.email@example.com>
2024-03-05 01:15:24 +0600

    add video player updated code

--- components/DetailsVideo/VideoPlayer/videoPlayer.jsx
+++ components/DetailsVideo/VideoPlayer/videoPlayer.jsx
@@ -2,8 +2,12 @@
 import React, { useRef, useState } from 'react';
 import { IoPlayForwardOutline } from "react-icons/io5";
 import { IoPlayBackOutline } from "react-icons/io5";
+import { CiPlay1 } from "react-icons/ci";
+
+
 
 import './player.css';
+import { IoIosPause } from 'react-icons/io';
 const VideoPlayer = ({ video ,handleVideoProgress}) => {
   const videoRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
@@ -28,15 +32,16 @@ const VideoPlayer = ({ video ,handleVideoProgress}) => {
       <video
         ref={videoRef}
         src={video}
-        controls// Hide default controls
+        controls
         onProgress={handleVideoProgress}
       />
       <div className="custom-controls">
-        <button className={`play-button ${isPlaying ? 'pause' : 'play'}`} onClick={togglePlay}>
-          {isPlaying ? 'Pause' : 'Play'}
-        </button>
         <button className='rewind-button' onClick={() => handleSeek(-10)}>
           <IoPlayBackOutline />
+        </button>
+        <button className={`play-button ${isPlaying ? 'Pause' : 'Play'}`} onClick={togglePlay}>
+          {isPlaying ? <IoIosPause /> : <CiPlay1 />
+}
         </button>
         <button className='forward-button' onClick={() => handleSeek(10)}>
           <IoPlayForwardOutline />
--- section/Footer.js
+++ section/Footer.js
@@ -2,9 +2,9 @@ const Footer = () => {
     return (
         <div>
             <footer class="bg-[#101010] text-white py-12">
-                <div class="container mx-auto flex flex-col md:flex-row lg:flex-row xl:flex-row 2xl:flex-row  justify-between">
+                <div class="container mx-auto flex flex-col md:flex-row lg:flex-row xl:flex-row 2xl:flex-row gap-4 justify-between">
                     <div class="max-w-96 ">
-                        <h3 class="text-base   text-center md:text-lg lg:text-lg xl:text-lg 2xl:text-2xl font-bold mb-4 text-yellow-500 ">About Us</h3>
+                        <h3 class="text-base ml-2  text-center md:text-lg lg:text-lg xl:text-lg 2xl:text-2xl font-bold mb-4 text-yellow-500 ">About Us</h3>
                         <p class="text-gray-300 ml-5 md:ml-1 lg:ml-5 xl:ml-1 2xl:ml-1 mb-3 text-center text-base md:text-base lg:text-lg xl:text-lg 2xl:text-lg">Your go-to destination for limitless entertainment. Discover, stream, and enjoy premium content effortlessly.</p>
                     </div>
                     <div>
